56ec67eb7ad310e361e8de2513d04cff55b175c1
adarshtp9633 <adarsh9633098135@gmail.com>
2025-01-23 11:06:54 +0530

    updated the index.js file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Adarsh" }');
+    res.send('{ "response": "Hello From Ad" }');
 });
 
 app.get('/will', function (req, res) {
